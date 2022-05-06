dc9e7d6a2579c3d80a601c3396df5391cdafa8f9
Dominic Walter <59534138+DominicwalterLOF@users.noreply.github.com>
2022-05-06 22:37:08 +0530

    update

--- web/grid.js
+++ web/grid.js
@@ -1,13 +1,28 @@
 var cols = 100;
-var rows = 100;
+var rows = 50;
 var rr = ""
 for (let r = 0; r < rows; r++) {
-    var inn = ""
+    var inn = "<div class = 'cell rowNumber' >" + r + "</div><input class= 'cell Name' value = 'NAME " + r + "' id = '" + r + "aN" + "'>"
+    if (r == 0) {
+        inn = "<div class = 'cell colNumber init' >Student Name</div>";
+    }
+    
     for (let c = 0; c < cols; c++) {
-        var temp = r + "a" + c
-        inn += "<input class= 'cell' value = " + temp + " id = '" + r + "a" + c + "'></input>"
+        if (r == 0) {
+            inn += "<div class = 'cell colNumber'>" + c + "</div>";
+        }
+        else {
+            var temp = r + "a" + c
+            inn += "<input class= 'cell' value = " + temp + " id = '" + temp + "'>"
+        }
+        
     }
     rr += "<div class = 'row'>" + inn + "</div>"
 }
 
 document.getElementById("main").innerHTML = rr;
+
+
+for (let i = 0; i < 5; i++) {
+    document.getElementById("cls").innerHTML += "<div class = 'classCard></div>'";
+}
